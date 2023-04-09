850c20bf0ff4988b0fb9a4a8f787755a326de437
aman <guptaaman61168@gmail.com>
2023-04-09 11:33:38 +0530

    Added KBC Project.

---
+++ 23_kbc_project.py
@@ -0,0 +1,284 @@
+print("🎉🤑👑WELCOME DEAR IN OUR KAON BANEGA CAROREPATI🎉🤑👑")
+print(input("Please first enter your name:"))
+print("very very thank you, Lets start now")
+print("Your first question is:\nWho is the founder of boat ?\n1.mr.elun musk           2.mr.bill gates\n3.mr.aman gupta          4.mr.gaotam adani")
+correctanswer = ("mr.aman gupta")
+wronganswer = ("1.mr.elun musk, 2.mr.bill gates, 4.mr.gaotam adani")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑5000🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 5000 RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 5000 RUPEES.")
+answer2 = input("Do you want life line yes/no: ")
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your second question is:\nWho is the ceo of google ?\n1.tarak mehta           2.mr.sundar pichayi\n3.mr.jalebi bhai          4.mr.gaotam adani")
+correctanswer = ("mr.sundar pichayi")
+wronganswer = ("1.tarak mehta , 2.mr.jalebi bhai, 4.mr.gaotam adani")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑1k🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1k RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1k RUPEES.")
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your third question is:\nWho is the main character of 12 fail nobel ?\n1.ips divya tanvar           2.sushil singh\n3.ias mr.manoj sharma          4.ias shrushti jayant deshmukh")
+correctanswer = ("ias mr.manoj sharma")
+wronganswer = ("1.ips divya tanvar, 2.sushil singh, 4.ias shrushti jayant deshmukh")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑50k🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 50k RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 50k RUPEES.")
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your fourth question is:\nWho is the founder of tata group ?\n1.mr.ratan tata           2.mr.mukesh ambani\n3.mr.aman gupta          4.mr.bill gates")
+correctanswer = ("mr.ratan tata")
+wronganswer = ("1.mr.elun musk, 2.mr.aman gupta, 4.mr.bill gates")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑1lakh🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1lakh RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1lakh RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your fifth question is:\nWho is the first president of India?\n1.mr.jawahar lal nehru           2.mr.rajendra prasad\n3.mr.Sardar Patel          4.Mahatma Gandhi")
+correctanswer = ("mr.rajendra prasad")
+wronganswer = ("1.mr.jawahar lal nehru 2.mr.Sardar Patel, 4.Mahatma Gandhi")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑1.5lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1.5lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1.5lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your sixth question is:\nWhat is the capital of India?\n1.bhutan           2.pakistan\n3.america          4.delhi")
+correctanswer = ("delhi")
+wronganswer = ("1.bhutan, 2.pakistan, 3.america")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑2lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 2lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 2lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your seventh question is:\nWhich country won the 2019 Cricket World Cup?\n1.england          2.south africa\n3.india          4.america")
+correctanswer = ("england")
+wronganswer = ("2.south africa, 3.india, 4.america")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑2.5lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 2.5lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 2.5lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your eighth question is:\nwho is inventer of areoplane?\n1.mr.elun musk           2.mr.bill gates\n3.the right brothers           4.mr.gaotam adani")
+correctanswer = ("mr.aman gupta")
+wronganswer = ("1.mr.elun musk, 2.mr.bill gates, 4.mr.gaotam adani")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑3lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 3lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 3lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your ninth question is:\nWhat is the name of Ram,s father?\n1.mr.kaotilya           2.mr.batasa vale\n3.mr.dasrath          4.mr.mahakaal")
+correctanswer = ("mr.dasrath")
+wronganswer = ("1.mr.kaotilya, 2.mr.batasa vale, 4.mr.mahakaal")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑10lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 10lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 10lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your tenth question is:\nWho is the chief minister of uttar pradesh?\n1.modi           2.akhilesh\n3.rahul          4.yogi baba")
+correctanswer = ("yogi baba")
+wronganswer = ("1.modi, 2.akhilesh, 4.rahul")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑20lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 20lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 20lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your eleventh question is:\nwhat is the national sweet of india?\n1.jalebi           2.rasgulla\n3.chhena          4.kaju katli")
+correctanswer = ("jalebi")
+wronganswer = ("1.rasgulla, 2.chhena, 4.kaju katli")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑50lakhs🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 50lakhs RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 50lakhs RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your twelevth question is:\nWho is enventer of bulb?\n1.thomas elva edison           2.elexander agraham bel\n3.a.p.j abdul kalam        4.subhash chandra bose")
+correctanswer = ("lion")
+wronganswer = ("1.lakadbagha, 2.kangaaro, 3.tiger")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑1crore🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1crore RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 1crore RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your thirteenth is:\nWh ?\n1.mr.elun musk           2.mr.bill gates\n3.mr.aman gupta          4.mr.gaotam adani")
+correctanswer = ("mr.aman gupta")
+wronganswer = ("1.mr.elun musk, 2.mr.bill gates, 4.mr.gaotam adani")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑3crore🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 3crore RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 3crore RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your fourteenth question is:\n what is the name of national flower of india ?\n1.chameli           2.lotus\n3.goldmarry          4.hebiscus")
+correctanswer = ("lotus")
+wronganswer = ("1.chameli, 2.goldmarry, 4.hebiscus")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑5crore🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 5crore RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 5crore RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
+
+print("Your first fifteenth is:\nWhat is the name of national bird of India ?\n1.banmurgi           2.gaoraya\n3.pecock          4.sparrow")
+correctanswer = ("pecock")
+wronganswer = ("1.banmurgi, 2.gaoraya, 4.sparrow")
+answer1 = (input("Please enter your answer:"))
+if(answer1==correctanswer):
+    print("👏💯😮👌CONGRATULATIONS👏💯😮👌\nYour answer is 💯% right\nyou wined 🎉🤑7crore🎉🤑 rupees.")
+elif(answer1==wronganswer):
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 7crore RUPEES.")           
+else:
+    print("😭SORRY😭\n❌Your answer is wrong.❌\nYOU LOST YOUR 7crore RUPEES.")
+
+answer2 = (input("Do you want life line yes/no:"))
+ans = ("yes")
+obj = ("no")
+if(answer2==ans):
+    print("1.Do you want to call a friend.\n2.Do you to take help of audiance.\nDo you want to call your teacher.")
+elif(answer2==obj):
+    print("ok")
